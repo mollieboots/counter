15a3defebb47cc539eb64257940d12e060b48e66
Mike Gallegos and Mollie Bootsma <student@epicodus.com>
2016-01-21 08:44:26 -0800

    add initial files

---
+++ spec/specs.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+// describe ('sanitizer', function() {
+// 	it("remove all non alphanumeric characters from input string", function() {
+// 		expect(sanitizer("Hello, Mike! Nice to meet you.")).to.equal("hellomikenicetomeetyou");
+// 	});
+// });
+//
+// describe ('reverseOrder', function() {
+// 	it("reverse the order of a string", function() {
+// 		expect(reverseOrder("123456789")).to.equal("987654321");
+// 	});
+// });
+//
+// describe ('PalindromeTester', function() {
+// 	it("Compare string to reversed version of string. If they are equal return true, else return false", function() {
+// 		expect(PalindromeTester("A dog, a plan, a canal: pagoda.")).to.equal(true);
+// 	});
+// });
+
+//
+// 	it ("is true for years divisible by 4", function() {
+// 		expect(leapYear(2004)).to.equal(true);
+// 	});
+//
+// 	it ("is false for years divisible by 100", function() {
+// 		expect (leapYear(1900)).to.equal(false);
+// 	});
+//
+// 	it ("is true for years divisible by 400", function() {
+// 		expect (leapYear(2000)).to.equal(true);
+// 	});
+//
+// 	it ("is false for blank input", function() {
+// 		expect (leapYear(NaN)).to.equal(false);
+// 	});
+//
+// 	it ("is false for negavtive integers", function() {
+// 		expect (leapYear(-400)).to.equal(false);
+// 	});
+// });
